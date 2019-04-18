032f59cf18ba781d078c6124b80a8f2250067911
Josh Kabo <joshkong1@gmail.com>
2019-04-18 16:38:40 -0700

    Set up Main

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+
+import javax.swing.*;
+
+public class Main extends JFrame implements GLEventListener {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void init(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
+
+    }
+}
