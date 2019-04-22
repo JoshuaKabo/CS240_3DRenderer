a78d91ac9f7bd2c8ff9a76d587886303cd98d14b
Josh Kabo <joshkong1@gmail.com>
2019-04-22 11:14:59 -0700

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame implements GLEventListener {
+
+    private int width = 800;
+    private int height = 600;
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+    }
+
+    public Main() {
+        super("Minimal OpenGL");
+        GLProfile profile = GLProfile.get(GLProfile.GL4);
+        GLCapabilities capabilities = new GLCapabilities(profile);
+
+        GLCanvas canvas = new GLCanvas(capabilities);
+
+        this.setName("3D Renderer");
+        this.getContentPane().add(canvas);
+        this.setSize(width, height);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setResizable(true);
+        canvas.requestFocusInWindow();
+
+    }
+
+    @Override
+    public void init(GLAutoDrawable glAutoDrawable) {
+        GL4 gl = glAutoDrawable.getGL().getGL4();
+        gl.glClearColor(0.392f, 0.584f, 0.929f, 1.0f);
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable glAutoDrawable) {
+        GL4 gl = glAutoDrawable.getGL().getGL4();
+        gl.glClear(GL4.GL_COLOR_BUFFER_BIT | GL4.GL_DEPTH_BUFFER_BIT);
+
+        gl.glFlush();
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
+
+    }
+}
