c71a17c4b99fab7ea47fd691259e8d6e1d2a0c41
Josh Kabo <joshkong1@gmail.com>
2019-04-25 15:13:30 -0700

    Added a Mesh interface
    All Mesh objects must implement getVertices(), which will be used when the camera draws rays to the vertices in the scene

--- src/Cube.java
+++ src/Cube.java
@@ -1,9 +1,14 @@
 import com.jogamp.graph.geom.Vertex;
 import com.jogamp.nativewindow.util.Point;
 
-public class Cube {
+public class Cube implements Mesh{
     private ProjectVertex vertex1 = new ProjectVertex(-1f, 1f,-1f);
     private ProjectVertex vertex2 = new ProjectVertex(-1f,-1f,-1f);
     private ProjectVertex vertex3 = new ProjectVertex(-1f,-1f,-1f);
     private ProjectVertex vertex4 = new ProjectVertex(-1f,-1f,-1f);
+
+    @Override
+    public ProjectVertex[] getVertices() {
+        return new ProjectVertex[0];
+    }
 }
---
+++ src/Mesh.java
@@ -0,0 +1,7 @@
+public interface Mesh {
+    /*
+    all mesh objects will have to reveal their vertices. In the future, the camera will ask for all vertices in the
+    scene and draw rays to them
+     */
+    public ProjectVertex[] getVertices();
+}
