c6f85b3b453584760321433095f78ebbbd24916a
Josh Kabo <joshkong1@gmail.com>
2019-04-25 15:53:15 -0700

    added eventlistener

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import com.jogamp.opengl.GL4;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.graph.geom.Vertex;
 import com.jogamp.opengl.*;
+import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.awt.GLCanvas;
+import javax.swing.JFrame;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Main extends JFrame implements GLEventListener {
+    private static final long serialVersionUID = 1L;
 
     private int width = 800;
     private int height = 600;
@@ -20,6 +27,7 @@ public class Main extends JFrame implements GLEventListener {
         GLCapabilities capabilities = new GLCapabilities(profile);
 
         GLCanvas canvas = new GLCanvas(capabilities);
+        canvas.addGLEventListener(this);
 
         this.setName("3D Renderer");
         this.getContentPane().add(canvas);
@@ -36,7 +44,7 @@ public class Main extends JFrame implements GLEventListener {
     public void init(GLAutoDrawable glAutoDrawable) {
         GL4 gl = glAutoDrawable.getGL().getGL4();
         //               Red      Green    Blue   Alpha
-        gl.glClearColor(0.0f, 0.0f, 1f, 1.0f);
+        gl.glClearColor(0.0f, 0.0f, 1f, 0.5f);
     }
 
     @Override
