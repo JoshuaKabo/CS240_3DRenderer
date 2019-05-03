916e7683ed950f87424b65e700ad6d2096dc044b
Josh Kabo <joshkong1@gmail.com>
2019-05-03 11:25:38 -0700

    Some Cube Work

--- src/Camera.java
+++ src/Camera.java
@@ -1,14 +1,18 @@
+import java.util.ArrayList;
+
 public class Camera {
 
     private ProjectVertex location;
     private ProjectVertex camDimensions;
     private Plane viewingPlane;
+    private float planeDist;
     public Camera()
     {
         this.camDimensions = new ProjectVertex(6f,6f,5f);
         this.location = new ProjectVertex(0f,0f,0f);
         viewingPlane = new Plane(camDimensions.getX(), camDimensions.getY());
         viewingPlane.translateZ(5);
+        planeDist = camDimensions.getZ();
     }
 
     /*
@@ -20,13 +24,17 @@ public class Camera {
 
     public void draw(Scene s)
     {
-        //this is n^2, might want to use a faster algorithm
+        ArrayList<ProjectVertex> projectedVertices = new ArrayList<>();
+        //this is n^2, might want to use a faster algorithm if possible
         Mesh[] meshes = s.getMeshes();
         for(Mesh mesh : meshes)
         {
             ProjectVertex[] vertices = mesh.getVertices();
             for(ProjectVertex vertex : vertices) {
-
+                projectedVertices.add( new ProjectVertex(
+                        (planeDist * (vertex.getX()/planeDist) ),
+                        (planeDist * (vertex.getX()/planeDist) ),
+                        planeDist) );
             }
         }
     }
--- src/Cube.java
+++ src/Cube.java
@@ -2,10 +2,48 @@ import com.jogamp.graph.geom.Vertex;
 import com.jogamp.nativewindow.util.Point;
 
 public class Cube implements Mesh{
-    private ProjectVertex vertex1 = new ProjectVertex(-1f, 1f,-1f);
-    private ProjectVertex vertex2 = new ProjectVertex(-1f,-1f,-1f);
-    private ProjectVertex vertex3 = new ProjectVertex(-1f,-1f,-1f);
-    private ProjectVertex vertex4 = new ProjectVertex(-1f,-1f,-1f);
+    private ProjectVertex[] vertices;
+    //[i][j]
+    //[i] represents the edge
+    //[j] represents the vertex numbers connected
+    private int[][] edgeData;
+    private float z = 10f;
+
+    //I want its origin at (0,0,z)
+    public Cube() {
+        vertices = new ProjectVertex[] {
+
+                //back face
+
+                //left, bottom, back
+                new ProjectVertex(-1, -1, z+1),
+                //right, bottom, back
+                new ProjectVertex(1, -1, z+1),
+                //right, top, back
+                new ProjectVertex(1, 1, z+1),
+                //left, top, back
+                new ProjectVertex(-1, 1, z+1),
+
+
+                //front face
+
+                //left, bottom, front
+                new ProjectVertex(-1, -1, z-1),
+                //right, bottom, front
+                new ProjectVertex(1, -1, z-1),
+                //right, top, front
+                new ProjectVertex(1, 1, z-1),
+                //left, top, front
+                new ProjectVertex(-1, 1, z-1),
+        };
+
+        edgeData = new int[][] {
+                //bottom back
+                {0,1},
+                //right back
+                {1,2},
+        };
+    }
 
     @Override
     public ProjectVertex[] getVertices() {
